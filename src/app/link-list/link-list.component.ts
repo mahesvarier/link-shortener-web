d21874be9bb4be43288470f4a651e608251f0a85
Mahes Varier <mahesvarier@gmail.com>
2024-11-05 21:34:38 +1100

    Adding a spinner to list component

--- src/app/link-list/link-list.component.ts
+++ src/app/link-list/link-list.component.ts
@@ -7,15 +7,20 @@ import { baseUrl } from 'src/config/config';
   templateUrl: './link-list.component.html',
   styleUrls: ['./link-list.component.css']
 })
-export class LinkListComponent implements OnInit{
+export class LinkListComponent implements OnInit {
   links: string[] = [];
   baseUrl: string = baseUrl;
+  isLoading = true;
 
   constructor(private linkService: LinkService) { }
 
   ngOnInit(): void {
     this.linkService.getAllLinks().subscribe(data => {
       this.links = data.map((link: string) => this.baseUrl + link);
+      this.isLoading = false;
+    }, error => {
+      console.error('Error fetching links', error);
+      this.isLoading = false;
     });
   }
 }
